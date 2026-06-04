febfe57b08af1e005ff0cd49bd489106f3338fa2
pratapbhanu812696-collab <pratapbhanu812696@gmail.com>
2026-06-04 19:45:05 +0530

    Update attendance tracker .py

--- Classwork/Iteration_4 june/attendance tracker .py
+++ Classwork/Iteration_4 june/attendance tracker .py
@@ -1,1 +1,6 @@
+#attendance tracker
+name = input("Enter your name: ")
+date = input("Enter the date (YYYY-MM-DD): ")
+status = input("Enter your attendance status (Present/Absent): ")
+print(f"Attendance record for {name} on {date}: {status}")
 
