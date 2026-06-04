182efcc9c3c36a2141f4c00aec578e7e0253f9dd
pratapbhanu812696-collab <pratapbhanu812696@gmail.com>
2026-06-04 21:38:54 +0530

    Update time coverter.py

--- Classwork/Selection_3 june/time coverter.py
+++ Classwork/Selection_3 june/time coverter.py
@@ -1,1 +1,8 @@
-
+#time converter 
+number = int(input("Enter the number of seconds: "))
+days = number // 86400
+hours = (number % 86400) // 3600
+minutes = (number % 3600) // 60
+seconds = number % 60
+print(f"{number} seconds is equal to {days} days, {hours} hours, {minutes} minutes, and {seconds} seconds.")
+ 
