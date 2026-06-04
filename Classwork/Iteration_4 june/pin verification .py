f1ea77978157b0dfe9c9f44bbd3b0d045148a516
pratapbhanu812696-collab <pratapbhanu812696@gmail.com>
2026-06-04 20:10:19 +0530

    Update pin verification .py

--- Classwork/Iteration_4 june/pin verification .py
+++ Classwork/Iteration_4 june/pin verification .py
@@ -1,1 +1,6 @@
+#pin verification 
+pin = input("Enter your 4-digit PIN: ")
+if len(pin) == 4 and pin.isdigit():
+    print("PIN accepted.")
+else:    print("Invalid PIN! Please enter a 4-digit numeric PIN.")
 
