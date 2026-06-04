63ee5efce75cc389eeebac26718eee604d99d54f
pratapbhanu812696-collab <pratapbhanu812696@gmail.com>
2026-06-04 19:49:40 +0530

    Update simple interest .py

--- Classwork/Selection_3 june/simple interest .py
+++ Classwork/Selection_3 june/simple interest .py
@@ -1,1 +1,9 @@
-
+#Calculating simple interest 
+p = float(input("Enter Principal Amount: "))
+r = float(input("Enter Rate of Interest (%): "))
+t = float(input("Enter Time (years): "))
+if p <= 0 or r <= 0 or t <= 0:
+    print("Invalid Input")
+else:
+    si = (p * r * t) / 100
+    print("Simple Interest =", si)
